0325aaac099bd839b2a20d6e559e7b6b7c712fbe
Arnav Tripathi <158262123+arnavt1605@users.noreply.github.com>
2025-06-24 20:30:47 +0530

    Update call.py

--- call.py
+++ call.py
@@ -1,15 +1,16 @@
 import os
 from twilio.rest import Client
 
+#Used environment variables for defining the variables
 account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
-    url="https://6b68-103-174-141-0.ngrok-free.app/voice",   
-    to="+919284834031",                
-    from_="+16802015441"              
+    url="---------",   #ngrok generated url
+    to="+91--------",  #Receiver's number
+    from_="+1--------"              
 )
 
 print(f"Started call: {call.sid}")
